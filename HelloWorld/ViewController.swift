35b73ad6fb015b655c5d0ddd2a929a59d52b5eb4
misaki1301 <paul.frankpc@gmail.com>
2022-10-22 17:22:08 -0500

    Grouped View with stack layout

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+	
+	@IBAction func showMessage(sender: UIButton) {
+		
+		let selectedButton = sender
+		
+		if let wordtToLookup = selectedButton.titleLabel?.text {
+			
+			var meaning = ""
+			
+			switch wordtToLookup {
+			case "👾":
+				meaning = "alien of space invaders"
+			case "🤓":
+				meaning = "nerd emoji"
+			case "🤖":
+				meaning = "robot emoji"
+			case "👻":
+				meaning = "ghost emoji"
+			default:
+				meaning = "XD"
+			}
+			
+			let alertController = UIAlertController(title: "Meaning", message: "\(meaning)", preferredStyle: .alert)
+			
+			alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+			
+			present(alertController, animated: true, completion: nil)
+		}
 	}
 
 
